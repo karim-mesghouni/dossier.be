c48731e4fcc429d8be10f4f86388aaac477e992e
B. Eboubaker <37766821+Eboubaker@users.noreply.github.com>
2021-09-04 15:15:44 +0100

    added address & contact list to client and clientInput

--- src/main/java/com/softline/dossier/be/graphql/types/input/ClientInput.java
+++ src/main/java/com/softline/dossier/be/graphql/types/input/ClientInput.java
@@ -1,5 +1,6 @@
 package com.softline.dossier.be.graphql.types.input;
 
+import com.softline.dossier.be.domain.Contact;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,7 +15,11 @@ import java.util.List;
 public class ClientInput  {
 
     private long id;
-    private  String Client;
+    private String name;
+    private String address;
     List<FileInput> files;
     List<VisAVisInput> visAVis;
+    List<ContactInput> contacts;
+
+
 }
