0a2e71ea410a29095a43deb88036a9ab452c5e22
B. Eboubaker <37766821+Eboubaker@users.noreply.github.com>
2021-09-04 15:18:23 +0100

    added find with files query

--- src/main/java/com/softline/dossier/be/repository/ClientRepository.java
+++ src/main/java/com/softline/dossier/be/repository/ClientRepository.java
@@ -9,4 +9,6 @@ import java.util.List;
 @Repository
 public interface ClientRepository   extends JpaRepository<Client,Long> {
     List<Client> findAllWithContactsByNameContaining(String search);
+
+    Client findWithFilesById(long id);
 }
