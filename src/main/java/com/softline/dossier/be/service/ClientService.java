7b46c7f28768411da4027688e97549afee3b1a8f
B. Eboubaker <37766821+Eboubaker@users.noreply.github.com>
2021-09-08 08:29:56 +0100

    contacts will be saved along the client

--- src/main/java/com/softline/dossier/be/service/ClientService.java
+++ src/main/java/com/softline/dossier/be/service/ClientService.java
@@ -1,26 +1,40 @@
 package com.softline.dossier.be.service;
 
 import com.softline.dossier.be.domain.Client;
+import com.softline.dossier.be.domain.Contact;
 import com.softline.dossier.be.graphql.types.input.ClientInput;
 import com.softline.dossier.be.repository.ClientRepository;
+import com.softline.dossier.be.repository.ContactRepository;
 import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 @Transactional
 
 @Service
 public class ClientService extends IServiceBase<Client, ClientInput, ClientRepository>
 {
+    @Autowired
+    ContactRepository contactRepository;
+
     @Override
     public List<Client> getAll() {
-        return  repository.findAll();
+        return repository.findAll();
     }
 
     @Override
     public Client create(ClientInput input) {
-        return repository.save(Client.builder().name(input.getName()).address(input.getAddress()).build());
+        Client client = Client.builder().name(input.getName()).address(input.getAddress()).build();
+        List<Contact> contacts = new ArrayList<>();
+        for (var contact: input.getContacts())
+        {
+            contacts.add(Contact.builder().name(contact.getName()).email(contact.getEmail()).phone(contact.getPhone()).client(client).build());
+        }
+        client.setContacts(contacts);
+        return repository.save(client);
     }
 
     @Override
@@ -28,6 +42,20 @@ public class ClientService extends IServiceBase<Client, ClientInput, ClientRepos
         Client client = repository.getOne(input.getId());
         client.setAddress(input.getAddress());
         client.setName(input.getName());
+        var clientContacts = client.getContacts();
+        for (var contactInput: input.getContacts())
+        {
+            var result = contactRepository.findById(contactInput.getId());
+            if(result.isEmpty())
+            {
+                client.addContact(Contact.builder().name(contactInput.getName()).email(contactInput.getEmail()).phone(contactInput.getPhone()).client(client).build());
+            }else{
+                var contact = client.findInContacts(e -> e.getId() == contactInput.getId());
+                contact.setName(contactInput.getName());
+                contact.setEmail(contactInput.getEmail());
+                contact.setPhone(contactInput.getPhone());
+            }
+        }
         return repository.save(client);
     }
 
