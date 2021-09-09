b3b34c3bbb0fb94654a67cba127f7f8ddc95d393
B. Eboubaker <37766821+Eboubaker@users.noreply.github.com>
2021-09-09 14:38:46 +0100

    add authorities to jwt token

--- src/main/java/com/softline/dossier/be/security/filters/AuthenticationFilter.java
+++ src/main/java/com/softline/dossier/be/security/filters/AuthenticationFilter.java
@@ -4,38 +4,43 @@ import com.auth0.jwt.JWT;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.softline.dossier.be.security.details.CustomAgentDetails;
 import com.softline.dossier.be.security.domain.Agent;
+import com.softline.dossier.be.security.domain.Privilege;
+import com.softline.dossier.be.security.domain.Role;
 import graphql.GraphQLException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
 import static com.softline.dossier.be.security.filters.constants.SecurityConstants.*;
 
-public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter
+{
     private AuthenticationManager authenticationManager;
 
-    public AuthenticationFilter(AuthenticationManager authenticationManager) {
+    public AuthenticationFilter(AuthenticationManager authenticationManager)
+    {
         this.authenticationManager = authenticationManager;
 
     }
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest req,
-                                                HttpServletResponse res) throws AuthenticationException {
-        try {
+                                                HttpServletResponse res) throws AuthenticationException
+    {
+        try
+        {
             Agent agent = new ObjectMapper()
                     .readValue(req.getInputStream(), Agent.class);
 
@@ -45,29 +50,42 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
                             agent.getPassword(),
                             Arrays.asList())
             );
-        } catch (IOException e) {
+        } catch (IOException e)
+        {
             throw new GraphQLException(e);
         }
 
     }
+
     @Override
     protected void successfulAuthentication(HttpServletRequest req,
                                             HttpServletResponse res,
                                             FilterChain chain,
-                                            Authentication auth) throws IOException {
+                                            Authentication auth) throws IOException
+    {
         String token = createToken(auth.getName(), auth);
         res.setHeader("Access-Control-Expose-Headers", HEADER_STRING);
-        res.addHeader(HEADER_STRING,  token);
+        res.addHeader(HEADER_STRING, token);
     }
-    private String createToken(String name, Authentication auth) {
-        List<String> grantedAuthorities = new ArrayList<>();
-        for (GrantedAuthority grantedAuthority : auth.getAuthorities()) {
-            grantedAuthorities.add(grantedAuthority.getAuthority());
-        }
+
+    private String createToken(String name, Authentication auth)
+    {
+        // get all privileges of agent passing throw its roles
+        Agent agent = ((CustomAgentDetails) auth.getPrincipal()).getAgent();
+        List<String> authorities = agent.getRoles()
+                .stream()
+                .flatMap(e -> e.getPrivileges()
+                        .stream()
+                        .map(Privilege::getName)
+                )
+                .distinct()
+                .collect(Collectors.toList());
+        // add all roles
+        authorities.addAll(agent.getRoles().stream().map(Role::getName).collect(Collectors.toList()));
         return JWT.create()
                 .withSubject(name)
-                .withClaim("grantedAuthorities", grantedAuthorities)
-                .withClaim("id", ((CustomAgentDetails)auth.getPrincipal()).getAgent().getId())
+                .withClaim("authorities", authorities)
+                .withClaim("id", agent.getId())
                 .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
                 .sign(HMAC512(SECRET.getBytes()));
     }
--- src/main/java/com/softline/dossier/be/security/filters/AuthorizationFilter.java
+++ src/main/java/com/softline/dossier/be/security/filters/AuthorizationFilter.java
@@ -9,7 +9,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
@@ -19,8 +18,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import static com.softline.dossier.be.security.filters.constants.SecurityConstants.*;
@@ -50,8 +48,8 @@ public class AuthorizationFilter extends BasicAuthenticationFilter {
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             }
         } catch (Exception e) {
-            logger.error("req ", e);
-            res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "hi");
+            logger.error("got invalid jwt token ", e);
+            res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "invalid token");
         }
         chain.doFilter(req, res);
 
@@ -64,26 +62,16 @@ public class AuthorizationFilter extends BasicAuthenticationFilter {
         if (token != null) {
             // parse the token.
 
-
             DecodedJWT decoded = JWT.require(Algorithm.HMAC512(SECRET.getBytes()))
                     .build()
                     .verify(token.replace(TOKEN_PREFIX, ""));
             String user = decoded.getSubject();
             Agent agent = Agent.builder().id(decoded.getClaim("id").asLong()).username(user).name(user).build();
             if (user != null) {
-                Collection<GrantedAuthority> grantedAuthorities = new ArrayList<>();
-                for (String authority : decoded.getClaim("grantedAuthorities").asList(String.class)) {
-
-                    grantedAuthorities.add(new SimpleGrantedAuthority(authority));
-
-                }
-                agent.setAuthorities(grantedAuthorities.stream().map(x -> x.getAuthority()).collect(Collectors.toList()));
-
-                return new UsernamePasswordAuthenticationToken(agent, null, grantedAuthorities);
+                List<String> authorities = decoded.getClaim("authorities").asList(String.class);
+                agent.setAuthorities(authorities);
+                return new UsernamePasswordAuthenticationToken(agent, null, authorities.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList()));
             }
-
-            return null;
-
         }
         return null;
     }
