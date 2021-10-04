bd30031e9b0adbf6866d5466c4dee2a20d6b72db
Eboubaker <37766821+Eboubaker@users.noreply.github.com>
2021-10-04 13:56:22 +0100

    added ability to send custom error message from server to the client.

--- src/main/java/com/softline/dossier/be/security/config/ExceptionsHandler.java
+++ src/main/java/com/softline/dossier/be/security/config/ExceptionsHandler.java
@@ -1,6 +1,7 @@
 package com.softline.dossier.be.security.config;
 
 import com.oembedler.moon.graphql.boot.error.ThrowableGraphQLError;
+import com.softline.dossier.be.service.exceptions.ClientReadableException;
 import graphql.GraphQLError;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Component;
@@ -10,6 +11,15 @@ import javax.persistence.EntityNotFoundException;
 
 @Component
 public class ExceptionsHandler {
+
+    /**
+     * this exception contains a written message by us,
+     * we will send it to the client to give a feedback message.
+     */
+    @ExceptionHandler(ClientReadableException.class)
+    public GraphQLError exceptionHandler(ClientReadableException e) {
+        return new ThrowableGraphQLError(e, e.getMessage());
+    }
 
     @ExceptionHandler(AccessDeniedException.class)
     public GraphQLError exceptionHandler(AccessDeniedException e) {
