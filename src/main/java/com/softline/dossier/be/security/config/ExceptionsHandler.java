3d694c93e213798e12c4986a81cdb7fc23ffd40e
Eboubaker <37766821+Eboubaker@users.noreply.github.com>
2021-10-30 15:35:24 +0100

    refactor exception handler

--- src/main/java/com/softline/dossier/be/security/config/ExceptionsHandler.java
+++ src/main/java/com/softline/dossier/be/security/config/ExceptionsHandler.java
@@ -3,6 +3,7 @@ package com.softline.dossier.be.security.config;
 import com.oembedler.moon.graphql.boot.error.ThrowableGraphQLError;
 import com.softline.dossier.be.service.exceptions.ClientReadableException;
 import graphql.GraphQLError;
+import graphql.GraphQLException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Component;
@@ -23,27 +24,30 @@ public class ExceptionsHandler
     public GraphQLError exceptionHandler(ClientReadableException e)
     {
         log.error("ClientReadableException: {}", e.getMessage());
-        return new ThrowableGraphQLError(e, e.getMessage());
+        return new ThrowableGraphQLError(e);
     }
 
     @ExceptionHandler(AccessDeniedException.class)
-    public GraphQLError exceptionHandler(AccessDeniedException e)
-    {
+    public GraphQLError exceptionHandler(AccessDeniedException e) {
         log.error("AccessDeniedException");
-        return new ThrowableGraphQLError(e, "Erreur de privilège");
+        return new ThrowableGraphQLError(new Throwable("Erreur de privilège"));
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public GraphQLError exceptionHandler(EntityNotFoundException e)
-    {
+    public GraphQLError exceptionHandler(EntityNotFoundException e) {
         log.error("EntityNotFoundException");
-        return new ThrowableGraphQLError(e, "Entité introuvable");
+        return new ThrowableGraphQLError(new Throwable("Entité introuvable"));
+    }
+
+    @ExceptionHandler(GraphQLException.class)
+    public GraphQLError exceptionHandler(GraphQLException e) {
+        log.error("GraphQLException: {}", e.getMessage());
+        return new ThrowableGraphQLError(new Throwable(e.getMessage()));
     }
 
     @ExceptionHandler(Throwable.class)
-    public GraphQLError exceptionHandler(Throwable e)
-    {
+    public GraphQLError exceptionHandler(Throwable e) {
         log.error("Throwable: {}", e.getMessage());
-        return new ThrowableGraphQLError(e, "server error");
+        return new ThrowableGraphQLError(new Throwable("server error"));
     }
 }
