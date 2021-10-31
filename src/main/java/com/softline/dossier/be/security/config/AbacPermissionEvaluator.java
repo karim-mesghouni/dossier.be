d7c620f3f5cd2ccbb97baa386fdc9ee4a2c1d135
Eboubaker <37766821+Eboubaker@users.noreply.github.com>
2021-10-31 09:56:47 +0100

    fix hasPermission() with targetType implementation

--- src/main/java/com/softline/dossier/be/security/config/AbacPermissionEvaluator.java
+++ src/main/java/com/softline/dossier/be/security/config/AbacPermissionEvaluator.java
@@ -1,8 +1,6 @@
 package com.softline.dossier.be.security.config;
 
 
-import com.softline.dossier.be.domain.File;
-import com.softline.dossier.be.domain.FileActivity;
 import com.softline.dossier.be.security.domain.Policy.PolicyEnforcement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +13,8 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+
+import static com.softline.dossier.be.Halpers.Functions.safeRun;
 
 @Component
 @RequiredArgsConstructor
@@ -36,12 +36,19 @@ public class AbacPermissionEvaluator implements PermissionEvaluator {
 
     @Override
     public boolean hasPermission(Authentication authentication, Serializable targetId, String targetType, Object permission) {
-        Object target = null;
-        if (targetType.equals("File")) {
-            target = entityManager.find(File.class, targetId);
-        } else if (targetType.equals("FileActivity")) {
-            target = entityManager.find(FileActivity.class, targetId);
-        }
-        return hasPermission(authentication, target, permission);
+        var ref = new Object() {
+            Object target = null;
+        };
+        this.entityManager.getMetamodel()
+                .getEntities()
+                .stream()
+                .filter(t -> t.getName().equals(targetType))
+                .findFirst()
+                .ifPresent(entityTarget ->
+                        safeRun(() ->
+                                ref.target = entityManager.find(entityTarget.getJavaType(), targetId)
+                        )
+                );
+        return hasPermission(authentication, ref.target, permission);
     }
 }
