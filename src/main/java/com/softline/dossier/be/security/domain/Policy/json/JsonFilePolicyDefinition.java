7928484835af0945358c590bfdf711f424bdaeba
Eboubaker <37766821+Eboubaker@users.noreply.github.com>
2021-11-03 11:14:13 +0100

    make policy for each role separate

--- src/main/java/com/softline/dossier/be/security/domain/Policy/json/JsonFilePolicyDefinition.java
+++ src/main/java/com/softline/dossier/be/security/domain/Policy/json/JsonFilePolicyDefinition.java
@@ -1,33 +1,26 @@
 package com.softline.dossier.be.security.domain.Policy.json;
 
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.softline.dossier.be.security.domain.Policy.PolicyDefinition;
 import com.softline.dossier.be.security.domain.Policy.PolicyRule;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.expression.Expression;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
+@Slf4j(topic = "PolicyLoader")
 public class JsonFilePolicyDefinition implements PolicyDefinition {
-    private static final Logger logger = LoggerFactory.getLogger(JsonFilePolicyDefinition.class);
-
-    private static final String DEFAULT_POLICY_FILE_NAME = "/default-policy.json";
-
-    @Value("${policy.json.filePath}")
-    private String policyFilePath;
-
     private List<PolicyRule> rules;
 
     @PostConstruct
@@ -37,22 +30,24 @@ public class JsonFilePolicyDefinition implements PolicyDefinition {
         module.addDeserializer(Expression.class, new SpelDeserializer());
         mapper.registerModule(module);
         try {
-            PolicyRule[] rulesArray;
-            logger.debug("[init] Checking policy file at: {}", policyFilePath);
-            if (policyFilePath != null && !policyFilePath.isEmpty()
-                    && Files.exists(Paths.get(policyFilePath))) {
-                logger.info("[init] Loading policy from custom file: {}", policyFilePath);
-                rulesArray = mapper.readValue(new File(policyFilePath), PolicyRule[].class);
-            } else {
-                logger.info("[init] Custom policy file not found. Loading default policy");
-                rulesArray = mapper.readValue(getClass().getResourceAsStream(DEFAULT_POLICY_FILE_NAME), PolicyRule[].class);
+            rules = new ArrayList<>();
+
+            ClassLoader cl = this.getClass().getClassLoader();
+            ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver(cl);
+            Resource[] resources = resolver.getResources("classpath:/policy/*.policy.json");
+            for (Resource resource : resources) {
+                try {
+                    log.debug("loading policy file: {}", resource.getFile());
+                    rules.addAll(Arrays.stream(mapper.readValue(resource.getInputStream(), PolicyRule[].class)).collect(Collectors.toList()));
+                } catch (IOException e) {
+                    log.error("An error occurred while parsing the policy file.", e);
+                }
             }
-            this.rules = (rulesArray != null ? Arrays.asList(rulesArray) : null);
-            logger.info("[init] Policy loaded successfully.");
-        } catch (JsonMappingException e) {
-            logger.error("An error occurred while parsing the policy file.", e);
         } catch (IOException e) {
-            logger.error("An error occurred while reading the policy file.", e);
+            log.error("Failed to load policy files.", e);
+        }
+        if (rules.isEmpty()) {
+            log.error("No policy json file was found in the class path: /policy/*.policy.json");
         }
     }
 
