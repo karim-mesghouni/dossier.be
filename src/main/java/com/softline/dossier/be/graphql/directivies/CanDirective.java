ab6833037437836cf250b68d53f9f0e6ce69b490
Eboubaker <37766821+Eboubaker@users.noreply.github.com>
2021-11-29 15:34:33 +0100

    bug fixes

--- src/main/java/com/softline/dossier/be/graphql/directivies/CanDirective.java
+++ src/main/java/com/softline/dossier/be/graphql/directivies/CanDirective.java
@@ -1,29 +1,46 @@
 package com.softline.dossier.be.graphql.directivies;
 
-//
-//public class CanDirective implements SchemaDirectiveWiring {
-//
-//    @Override
-//    public GraphQLFieldDefinition onField(SchemaDirectiveWiringEnvironment<GraphQLFieldDefinition> environment) {
-//        GraphQLFieldDefinition field = environment.getElement();
-//        String action = (String) environment.getDirective().getArgument("action").getValue();
-//
-//        GraphQLFieldsContainer parentType = environment.getFieldsContainer();
-//        //
-//        // build a data fetcher that first checks authorisation roles before then calling the original data fetcher
-//        //
-//        var original = environment.getCodeRegistry().getDataFetcher(parentType, field);
-//        DataFetcherFactories.wrapDataFetcher(field.getDataFetcher()
-//        DataFetcher<?> authDataFetcher = dataFetchingEnvironment -> {
-//            if (AttributeBasedAccessControlEvaluator.can(action,null)) {
-//                return original.get(dataFetchingEnvironment);
-//            } else {
-//                return null;
-//            }
-//        };
-//        //
-//        // now change the field definition to have the new authorising data fetcher
-//        environment.getCodeRegistry().dataFetcher(parentType, field, authDataFetcher);
-//        return field.transform(builder -> builder.dataFetcher(authDataFetcher));
-//    }
-//}
+
+import com.softline.dossier.be.database.Database;
+import com.softline.dossier.be.domain.Concerns.HasId;
+import com.softline.dossier.be.security.config.AttributeBasedAccessControlEvaluator;
+import graphql.Scalars;
+import graphql.schema.*;
+import graphql.schema.idl.SchemaDirectiveWiring;
+import graphql.schema.idl.SchemaDirectiveWiringEnvironment;
+import org.modelmapper.ModelMapper;
+
+import static com.softline.dossier.be.Application.getBean;
+
+public class CanDirective implements SchemaDirectiveWiring {
+
+    @Override
+    public GraphQLFieldDefinition onField(SchemaDirectiveWiringEnvironment<GraphQLFieldDefinition> environment) {
+        GraphQLFieldDefinition field = environment.getElement();
+        GraphQLFieldsContainer parentType = environment.getFieldsContainer();
+        String action = (String) environment.getDirective().getArgument("action").getValue();
+        String entity = (String) environment.getDirective().getArgument("entity").getValue();
+
+        DataFetcher originalFetcher = environment.getCodeRegistry().getDataFetcher(parentType, field);
+        DataFetcher dataFetcher = dataFetchingEnvironment -> {
+            var in = getBean(ModelMapper.class).map(dataFetchingEnvironment.getArguments().values().stream().findFirst().orElseThrow(), Database.getEntityType(entity).getJavaType());
+            var out = Database.findOrThrow(Database.getEntityType(entity).getJavaType(), ((HasId) in).getId());
+            AttributeBasedAccessControlEvaluator.DenyOrProceed(action, out);
+            return out;
+        };
+        //
+        // now change the field definition to have the new authorising data fetcher
+        FieldCoordinates coordinates = FieldCoordinates.coordinates(parentType, field);
+        environment.getCodeRegistry().dataFetcher(coordinates, dataFetcher);
+        return field.transform(builder -> builder
+                .argument(GraphQLArgument
+                        .newArgument()
+                        .name("action")
+                        .type(Scalars.GraphQLString)
+                ).argument(GraphQLArgument
+                        .newArgument()
+                        .name("entity")
+                        .type(Scalars.GraphQLString))
+        );
+    }
+}
--- src/main/java/com/softline/dossier/be/graphql/directivies/Registry.java
+++ src/main/java/com/softline/dossier/be/graphql/directivies/Registry.java
@@ -1,9 +1,13 @@
 package com.softline.dossier.be.graphql.directivies;
 
-//@Component
-//public class Registry {
-//    @Bean
-//    public SchemaDirective canDirective() {
-//        return new SchemaDirective("can", new CanDirective());
-//    }
-//}
+import com.oembedler.moon.graphql.boot.SchemaDirective;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Registry {
+    @Bean
+    public SchemaDirective canDirective() {
+        return new SchemaDirective("can", new CanDirective());
+    }
+}
