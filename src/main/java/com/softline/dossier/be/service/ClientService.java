5df2c58586d7b3ee60be546501ec6dfe2f682a08
Eboubaker Bekkouche <eboubakkar@gmail.com>
2022-01-25 08:08:08 +0100

    bug fix.

--- src/main/java/com/softline/dossier/be/service/ClientService.java
+++ src/main/java/com/softline/dossier/be/service/ClientService.java
@@ -1,15 +1,18 @@
 package com.softline.dossier.be.service;
 
+import com.softline.dossier.be.Tools.Functions;
 import com.softline.dossier.be.database.Database;
 import com.softline.dossier.be.domain.Client;
 import com.softline.dossier.be.domain.Contact;
 import com.softline.dossier.be.events.EntityEvent;
 import com.softline.dossier.be.events.entities.ClientEvent;
 import com.softline.dossier.be.graphql.types.input.ClientInput;
+import graphql.GraphQLException;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ClientService {
@@ -29,10 +32,26 @@ public class ClientService {
     @PreAuthorize("hasPermission(null, 'CREATE_CLIENT')")
     public Client create(Client client) {
         Database.startTransaction();
+        this.validateClientInput(client);
+        client.setId(null);
+        client.getContacts().removeIf(ct -> ct.getEmail() == null && ct.getName() == null && ct.getPhone() == null);
+        client.getContacts().forEach(ct -> ct.setId(null));
         Database.persist(client);
         Database.commit();
         new ClientEvent(EntityEvent.Type.ADDED, client).fireToAll();
         return client;
+    }
+
+    private void validateClientInput(Client client) {
+        Functions.throwIfEmpty(client.getName(), () -> new GraphQLException("le nom du client est requis"));
+        client.getContacts()
+                .stream()
+                .filter(ct -> ct.getId() != null && ct.getId() != 0)
+                .collect(Collectors.toList()).
+                forEach(ct -> {
+                    if (Functions.isEmpty(ct::getEmail) && Functions.isEmpty(ct::getName) && Functions.isEmpty(ct::getPhone))
+                        throw new GraphQLException("contact ne peut pas être vide");
+                });
     }
 
     public Client update(ClientInput input) {
@@ -50,6 +69,7 @@ public class ClientService {
                     contact.setPhone(contactInput.getPhone());
                 }
             }
+            this.validateClientInput(client);
             Database.commit();
             new ClientEvent(EntityEvent.Type.UPDATED, client).fireToAll();
             return client;
